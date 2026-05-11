a4724a38fc4745fb8d077fa683ce0db91e699a3b
Jacob Chun <jacobkchun@gmail.com>
2026-05-10 22:25:54 -0700

    Added helper files

---
+++ ros2_ws/src/robot/robot/fsm_helpers/firmware_helpers.py
@@ -0,0 +1,31 @@
+from robot.hardware_map import (
+    INITIAL_THETA_DEG,
+    LEFT_WHEEL_DIR_INVERTED,
+    LEFT_WHEEL_MOTOR,
+    POSITION_UNIT,
+    RIGHT_WHEEL_DIR_INVERTED,
+    RIGHT_WHEEL_MOTOR,
+    WHEEL_BASE,
+    WHEEL_DIAMETER,
+    TAG_ID
+)
+from robot.robot import FirmwareState, Robot
+
+def configure_robot(robot: Robot) -> None:
+    robot.set_unit(POSITION_UNIT)
+    robot.set_odometry_parameters(
+        wheel_diameter=WHEEL_DIAMETER,
+        wheel_base=WHEEL_BASE,
+        initial_theta_deg=INITIAL_THETA_DEG,
+        left_motor_id=LEFT_WHEEL_MOTOR,
+        left_motor_dir_inverted=LEFT_WHEEL_DIR_INVERTED,
+        right_motor_id=RIGHT_WHEEL_MOTOR,
+        right_motor_dir_inverted=RIGHT_WHEEL_DIR_INVERTED,
+    )
+    robot.set_tracked_tag_id(TAG_ID)
+
+
+def start_robot(robot: Robot) -> None:
+    robot.set_state(FirmwareState.RUNNING)
+    robot.reset_odometry()
+    robot.wait_for_pose_update(timeout=0.2)
---
+++ ros2_ws/src/robot/robot/fsm_helpers/task_planner.py
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------
+# Venue placement configuration (TODO: Replace w/ real values or automate)
+# ---------------------------------------------------------------------------
+TOLERANCE_MM = 20 # TODO: Tune
+
+POSE_TRAFFIC_LIGHT = (0, 0, 90)
+POSE_BURGER_PATTY = (0, 1828.8, 180)
+POSE_BURGER_BUN = (0, 1219.2, 180)
+POSE_CUSTOMER_TARGET = (2438.4, 3048, -90)
+POSE_CUSTOMER_A = (2438.4, 2438.4, 0)
+POSE_STOP_SIGN = (2438.4, 609.6, -90)
+
+# ----- Tasks ---------------------------------------------------------------
+
+tasks = [
+    {"state": "WAIT", "trigger": "green_light", "goal_pose_mm": POSE_TRAFFIC_LIGHT},
+    {"state": "NAV",  "visualTarget": "traffic_light"},
+    {"state": "NAV",  "visualTarget": "burger_bun", "goal_pose_mm": POSE_BURGER_BUN},
+    {"state": "MANIP", "cmd": "pick"},
+    {"state": "NAV",  "visualTarget": "burger_patty", "goal_pose_mm": POSE_BURGER_PATTY},
+    {"state": "MANIP", "cmd": "pick"},
+    {"state": "NAV",  "visualTarget": "burger_bun", "goal_pose_mm": POSE_BURGER_BUN},
+    {"state": "MANIP", "cmd": "pick"},
+    {"state": "NAV",  "visualTarget": "customer_shown", "goal_pose_mm": POSE_CUSTOMER_TARGET},
+    {"state": "IDENT"},
+    {"state": "NAV",  "visualTarget": "customer", "goal_pose_mm": POSE_CUSTOMER_A},
+    {"state": "MANIP", "cmd": "place"},
+    {"state": "NAV",  "visualTarget": "stop_sign", "goal_pose_mm": POSE_STOP_SIGN},
+    {"state": "DONE"}
+]
