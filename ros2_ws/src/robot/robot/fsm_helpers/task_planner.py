6311088d43ff59d36c3bf6035aaa3199c8ac440a
Jacob Chun <jacobkchun@gmail.com>
2026-06-05 01:30:14 -0700

    Changed test

--- ros2_ws/src/robot/robot/fsm_helpers/task_planner.py
+++ ros2_ws/src/robot/robot/fsm_helpers/task_planner.py
@@ -5,7 +5,7 @@ User-defined mission tasks. Set MODE below
 from robot.fsm_helpers import course_parameters as cp
 from robot.fsm_helpers.course_parameters import SQ
 
-MODE = "manip_nav"
+MODE = "helpers"
 # MODE = "RANDOM"
 
 if MODE == "RANDOM":
@@ -94,12 +94,10 @@ elif MODE == "TUNE_LAPF":
 
 elif MODE == "helpers":
     tasks = [
+        {"state": "HOME"},
         {"state": "MANIP", "command": "pick", "ingredient": "bun"},
-        {"state": "IDENT"},
         {"state": "MANIP", "command": "pick", "ingredient": "patty"},
-        {"state": "IDENT"},
         {"state": "MANIP", "command": "pick", "ingredient": "bun"},
-        {"state": "IDENT"},
         {"state": "MANIP", "command": "place"}
     ]
 
@@ -202,25 +200,17 @@ elif MODE == "FINAL":
     tasks = [
         {"state": "WAIT", "trigger": "green_light"},
         {"state": "NAV", "path_planner": "pp", "vision": "burger_bun", "waypoints": cp.WP_BURGER_BUN_1},
-        {"state": "MANIP", "cmd": "pick"},
-        {"state": "NAV", "path_planner": "pp", "vision": "burger_patty", "waypoints": cp.WP_BURGER_PATTY},
-        {"state": "MANIP", "cmd": "pick"},
-        {"state": "NAV", "path_planner": "pp", "vision": "burger_bun", "waypoints": cp.WP_BURGER_BUN_2},
-        {"state": "MANIP", "cmd": "pick"},
-        {"state": "NAV", "path_planner": "pp", "waypoints": [cp.WP1, cp.WP2, cp.WP3, cp.WP4]},
-        {"state": "NAV", "path_planner": "lapf", "vision": "face", "waypoints": cp.WP_CUSTOMER_ID},
-        {"state": "IDENT"},
-        {"state": "PLAN"},
-        {"state": "NAV", "path_planner": "pp", "vision": "customer", "waypoints": cp.WP_CUSTOMER_1},
-        {"state": "MANIP", "cmd": "place"},
-        {"state": "NAV", "path_planner": "pp", "vision": "stop_sign", "waypoints": cp.WP_STOP_SIGN},
-        {"state": "NAV", "path_planner": "pp", "waypoints": cp.WP5}
+        # {"state": "MANIP", "cmd": "pick"},
+        # {"state": "NAV", "path_planner": "pp", "vision": "burger_patty", "waypoints": cp.WP_BURGER_PATTY},
+        # {"state": "MANIP", "cmd": "pick"},
+        # {"state": "NAV", "path_planner": "pp", "vision": "burger_bun", "waypoints": cp.WP_BURGER_BUN_2},
+        # {"state": "MANIP", "cmd": "pick"},
+        # {"state": "NAV", "path_planner": "pp", "waypoints": [cp.WP1, cp.WP2, cp.WP3, cp.WP4]},
+        # {"state": "NAV", "path_planner": "lapf", "vision": "face", "waypoints": cp.WP_CUSTOMER_ID},
+        # {"state": "IDENT"},
+        # {"state": "PLAN"},
+        # {"state": "NAV", "path_planner": "pp", "vision": "customer", "waypoints": cp.WP_CUSTOMER_1},
+        # {"state": "MANIP", "cmd": "place"},
+        # {"state": "NAV", "path_planner": "pp", "vision": "stop_sign", "waypoints": cp.WP_STOP_SIGN},
+        # {"state": "NAV", "path_planner": "pp", "waypoints": cp.WP5}
     ]
-
-tasks = [
-    {
-        "state": "NAV",
-        "path_planner": "pp",
-        "waypoints": [(0, 10*24*25.4)]
-    }
-]
